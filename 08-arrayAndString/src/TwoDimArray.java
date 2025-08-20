d39d90278feb4c6777eb23d8e64652e762d93844
esmacimsit <esmanurcimsit@gmail.com>
2025-08-21 01:10:43 +0300

    array done

--- 08-arrayAndString/src/TwoDimArray.java
+++ 08-arrayAndString/src/TwoDimArray.java
@@ -1,8 +1,22 @@
+import java.util.Arrays;
+
 public class TwoDimArray {
     public static void main(String[] args) {
         int[][] x = new int[5][5]; // 5x5 matrix
         x[2][1] = 8;
         int[][] y = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}; // 3x3 matrix
 
+        System.out.println("Matrix: " + Arrays.deepToString(y));
+        System.out.println(y[1][1]); // 5
+        System.out.println("***");
+
+        for (int i = 0; i < y.length; i++){
+            for (int j = 0; j < y[i].length; j++) {
+                System.out.println(y[i][j] + " ");
+            }
+        }
+
+        int[][][] z = {{{1, 2}, {3, 4}}, {{5, 6}, {7, 8}}};
+        System.out.println("3D array: " + Arrays.deepToString(z));
     }
 }
